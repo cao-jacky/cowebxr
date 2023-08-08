49f869e98f0da54557d35867254fdffa15f4848d
Jacky Cao <jackycao@outlook.com>
2023-08-08 16:53:25 +0300

    depth client can offload to server

--- server/socket_receiver.py
+++ server/socket_receiver.py
@@ -5,14 +5,48 @@ import pathlib
 import ssl
 import websockets
 
-async def hello(websocket):
-    name = await websocket.recv()
-    print(f"<<< {name}")
+import json
 
-    greeting = f"Hello {name}!"
+import open3d as o3d
+import numpy as np
 
-    await websocket.send(greeting)
-    print(f">>> {greeting}")
+from matplotlib import pyplot as plt
+
+# set array for depth data
+depth_2d_array = None
+
+def depth_analyser(client_data):
+    depth_data = client_data['depth_data']
+        
+    data = depth_data['data']
+    
+    depth_height = depth_data['height']
+    depth_width = depth_data['width']
+    
+    # print(np.shape(data), depth_height, depth_width)
+    
+    if np.sum(data) != 0: 
+        depth_2d_array = np.reshape(data, (depth_height, depth_width))
+        depth_2d_array = np.fliplr(np.transpose(depth_2d_array))
+        # print(depth_2d_array)
+        
+        plt.imshow(depth_2d_array, interpolation='nearest')
+        # plt.camroll(-90)
+        plt.savefig('foo.png')
+
+async def receive(websocket):
+    received_data = await websocket.recv()
+    try:
+        data_json = json.loads(received_data)
+        depth_analyser(data_json)
+        # print(data_json['depth_data'])
+    except:
+        pass
+    # print(f"<<< {name}")
+
+    # greeting = f"Hello {name}!"
+    # await websocket.send(greeting)
+    # print(f">>> {greeting}")
 
 ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
 # localhost_pem = pathlib.Path(__file__).with_name("localhost.pem")
@@ -24,7 +58,7 @@ cowebxr_key = pathlib.Path(__file__).with_name("cowebxr.com+3-key.pem")
 ssl_context.load_cert_chain(cowebxr_pem, keyfile=cowebxr_key, password=None)
 
 async def main():
-    async with websockets.serve(hello, "0.0.0.0", 8765, ssl=ssl_context):
+    async with websockets.serve(receive, "0.0.0.0", 8765, ssl=ssl_context):
         await asyncio.Future()  # run forever
 
 if __name__ == "__main__":
